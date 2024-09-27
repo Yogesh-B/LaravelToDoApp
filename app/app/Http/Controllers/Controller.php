937db44e34fcd082b233ccebc897934d54e3d827
yogesh-b <yogeshbavishi199@gmail.com>
2024-09-27 19:36:34 +0530

    docs: swagger started

--- app/app/Http/Controllers/Controller.php
+++ app/app/Http/Controllers/Controller.php
@@ -6,6 +6,38 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *      @OA\Info(
+ *          version="3.0.0",
+ *          title="Todo App Api Docs",
+ *          description="This is official docs for the Todo App",
+ *          version="0.0.1"
+ *      ),
+ *      @OA\Server(
+ *          description="Staging server",
+ *          url="https://staging.laravel-todo.me"
+ *      ),
+ *      @OA\Server(
+ *          description="Production server",
+ *          url="https://laravel-todo.me"
+ *      ),
+ * 
+ *      @OA\Get(
+ *          path="/sample-path",
+ *          summary="This is just a sample path to generate docs",
+ *          operationId="samplePath",
+ *          parameters={},
+ *          @OA\Response(
+ *              response=200,
+ *              description="successful operation",
+ *          ),
+ *      ),
+ * )
+ */
+
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
