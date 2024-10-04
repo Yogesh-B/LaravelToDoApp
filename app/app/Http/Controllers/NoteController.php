b4272167009084c9849d6477b4f2000f78898346
yogesh-b <yogeshbavishi199@gmail.com>
2024-10-04 18:11:08 +0530

    chore: some formatting

--- app/app/Http/Controllers/NoteController.php
+++ app/app/Http/Controllers/NoteController.php
@@ -11,48 +11,48 @@ use Illuminate\Http\Response;
 
 class NoteController extends Controller
 {
-        public function index(Request $request){
-            $perPage = $request->input('per_page', 15);
-    
-            return new SuccessResponse(new NoteCollection(Note::paginate($perPage)),"Notes fetched");
-        }
-    
-    
-        public function show(Note $note){
-            return new SuccessResponse($note,"Note retrieved");
-        }
+    public function index(Request $request){
+        $perPage = $request->input('per_page', 15);
+
+        return new SuccessResponse(new NoteCollection(Note::paginate($perPage)),"Notes fetched");
+    }
     
     
-        public function store(RecordList $recordList, Request $request){
-
-            $listId = $recordList->id;
-
-            $recordList = Note::create([
-                'list_id'=> $listId,
-                'title'=>$request->input('title',""),
-                'description'=>$request->input('description'),
-                'is_completed'=>$request->input('is_completed',false),
-            ]);
+    public function show(Note $note){
+        return new SuccessResponse($note,"Note retrieved");
+    }
     
-            return new SuccessResponse(["id"=>$recordList->id],"Note created successfully",Response::HTTP_CREATED);
-        }
+    
+    public function store(RecordList $recordList, Request $request){
+
+        $listId = $recordList->id;
+
+        $recordList = Note::create([
+            'list_id'=> $listId,
+            'title'=>$request->input('title',""),
+            'description'=>$request->input('description'),
+            'is_completed'=>$request->input('is_completed',false),
+        ]);
+
+        return new SuccessResponse(["id"=>$recordList->id],"Note created successfully",Response::HTTP_CREATED);
+    }
 
 
-        public function update(Request $request, Note $note){
-            $note->update([
-                'title'=>$request->input('title',""),
-                'description'=>$request->input('description'),
-                'is_completed'=>$request->input('is_completed',false),
-            ]);
+    public function update(Request $request, Note $note){
+        $note->update([
+            'title'=>$request->input('title',""),
+            'description'=>$request->input('description'),
+            'is_completed'=>$request->input('is_completed',false),
+        ]);
 
-            return new SuccessResponse(["note"=>$note],"Note updated successfully",Response::HTTP_ACCEPTED);
-        }
+        return new SuccessResponse(["note"=>$note],"Note updated successfully",Response::HTTP_ACCEPTED);
+    }
 
 
-        public function destroy(Note $note){
-            $note->delete();
+    public function destroy(Note $note){
+        $note->delete();
 
-            return new SuccessResponse(["id"=>$note->id],"Note deleted successfully",Response::HTTP_OK);
-        }
+        return new SuccessResponse(["id"=>$note->id],"Note deleted successfully",Response::HTTP_OK);
+    }
 
 }
