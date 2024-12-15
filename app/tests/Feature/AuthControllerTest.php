8a18870658f2806f5112846e821df503cafb11a9
yogesh-b <yogeshbavishi199@gmail.com>
2024-12-15 16:05:47 +0530

    test: added tests for AuthController

--- app/tests/Feature/AuthControllerTest.php
+++ app/tests/Feature/AuthControllerTest.php
@@ -5,13 +5,14 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+
+    //login works
     public function test_login_works(): void
     {
         $user = User::factory()->create();
@@ -21,29 +22,116 @@ class AuthControllerTest extends TestCase
             'password' => 'password'
         ];
 
-        $response = $this->post('/api/login',$payload);
+        $response = $this->post('/api/login', $payload);
 
         $response->assertStatus(200);
     }
 
-    //login works
     //login validation works
+    public function test_login_validation_works(): void
+    {
+        $payload = [
+            'email' => 'invalidemail.com',
+            'password' => ''
+        ];
+
+        $response = $this->post('/api/login', $payload);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertStringContainsString('must be a valid email address', $response->getContent());
+        $this->assertStringContainsString('password field is required', $response->getContent());
+    }
+
+
     //login attempt fails for non-registered user
-    //login fails for wrong email/password
-    
+    public function test_login_fails_for_non_registered_user(): void
+    {
+        $payload = [
+            'email' => '7o6H0@example.com',
+            'password' => 'password'
+        ];
+
+        $response = $this->post('/api/login', $payload);
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $this->assertStringContainsString("Invalid credentials", $response->getContent());
+    }
 
     //register works
+    public function test_register_works(): void
+    {
+        $payload = [
+            'name' => 'John Doe',
+            'email' => '7o6H0@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->post('/api/register', $payload);
+
+        $response->assertJsonStructure([
+            "success",
+            "message",
+            "access_token",
+            "token_type",
+            "expires_in",
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Doe',
+            'email' => '7o6H0@example.com'
+        ]);
+    }
+
     //register validation works
-    //responds with token
-    //gives error for same email
+    public function test_register_validation_works(): void
+    {
+        $payload = [
+            "name" => "",
+            "email" => "",
+            "password" => "",
+        ];
 
+        $response = $this->post('/api/register', $payload);
 
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertStringContainsString('name field is required', $response->getContent());
+        $this->assertStringContainsString('email field is required', $response->getContent());
+        $this->assertStringContainsString('password field is required', $response->getContent());
+    }
+
+    // /registration fails for already registered user
+    public function test_register_fails_for_already_registered_user(): void
+    {
+        $user = User::factory()->create();
+
+        $payload = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+
+        $response = $this->post('/api/register', $payload);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertStringContainsString('email has already been taken', $response->getContent());
+    }
 
     //logout works
+    public function test_logout_works(): void
+    {
+        $user = User::factory()->create();
+
+        $token = Auth::login($user);
+        
+        $response = $this->withHeader('Authorization','Bearer ' . $token)->post('/api/logout');
+        $response->assertJsonFragment(["message"=>"Logout successful"]);
+    }
 
     //refresh works
-        //carbon time manipulation for getting expired error in response
-        //fail -> work
+    //carbon time manipulation for getting expired error in response
+    //fail -> work
 
 
 
