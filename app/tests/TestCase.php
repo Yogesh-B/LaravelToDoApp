442e484e77204da07ba4bbde1b53f120449efbf1
yogesh-b <yogeshbavishi199@gmail.com>
2024-12-15 17:47:25 +0530

    test: changes in base class
    previous method was causing data to stay in database
    across test cases. Now data is not persisted across test
    cases and speed is as fast as previous method

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
+// use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
@@ -10,21 +12,22 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     // use DatabaseMigrations;
-    use DatabaseTransactions;
+    use RefreshDatabase;
+    // use DatabaseTransactions;
 
     //Thanks to ChatGPT
-    protected static $migrated = false;
+    // protected static $migrated = false;
 
-    public function setUp(): void
-    {
-        parent::setUp();
+    // public function setUp(): void
+    // {
+    //     parent::setUp();
 
-        // Run migrations only once
-        if (!static::$migrated) {
-            Artisan::call('migrate:fresh');
-            static::$migrated = true;
-        }
-    }
+    //     // Run migrations only once
+    //     if (!static::$migrated) {
+    //         Artisan::call('migrate:fresh');
+    //         static::$migrated = true;
+    //     }
+    // }
 
     
 }
