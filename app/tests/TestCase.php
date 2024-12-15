877780a9907b1b6bba1d957ac3f8e8138bb3db3d
yogesh-b <yogeshbavishi199@gmail.com>
2024-12-15 16:00:47 +0530

    test: speed up test case run time

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,14 +2,29 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use DatabaseMigrations;
+    // use DatabaseMigrations;
     use DatabaseTransactions;
+
+    //Thanks to ChatGPT
+    protected static $migrated = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations only once
+        if (!static::$migrated) {
+            Artisan::call('migrate:fresh');
+            static::$migrated = true;
+        }
+    }
+
     
 }
